4e635dcf1dd3c1553d4e8555936a8b82a37cc49f
NicholasAG1 <70723112+NicholasAG1@users.noreply.github.com>
2020-09-10 14:27:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,42 +3,40 @@
 // Listen for key events on the page
 document.addEventListener("keydown", playSound);
 
-
-
 // Event Function
 function playSound(event) {
     console.log(event.keyCode);
 
     if (event.keyCode == 65) { // "a"
         // Play A-Clap
-        palyDrum(clap, A)
+        palyDrum('clap', 'A')
     } else if (event.keyCode == 83) { // "s"
         // Play S-hihat
-        palyDrum(hihat, S)
+        palyDrum('hihat', 'S')
     } else if (event.keyCode == 68) { // "d"
         // Play D-kick
-        palyDrum(kick, D)
+        palyDrum('kick', 'D')
     } else if (event.keyCode == 70) { // "f"
         // Play F-Openhat
-        palyDrum(openhat, F)
+        palyDrum('openhat', 'F')
     } else if (event.keyCode == 71) { // "g"
         // Play G-Boom
-        palyDrum(boom, G)
+        palyDrum('boom', 'G')
     } else if (event.keyCode == 72) { // "h"
         // Play H-Ride
-        palyDrum(ride, H)
+        palyDrum('ride', 'H')
     } else if (event.keyCode == 74) { // "j"
         // Play J-Snare
-        palyDrum(snare, J)
+        palyDrum('snare', 'J')
     } else if (event.keyCode == 75) { // "k"
         // Play K-Tom
-        palyDrum(tom, K)
+        palyDrum('tom', 'K')
     } else if (event.keyCode == 76) { // "l"
         // Play L-Tink
-        palyDrum(tink, L)
+        palyDrum('tink', 'L')
     }
 }
-
+//play the sound
 function palyDrum(name, letter,) {
     let audio = document.getElementById(name);
     audio.currentTime = 0;
@@ -48,11 +46,7 @@ function palyDrum(name, letter,) {
     document.getElementById(letter).addEventListener('transitionend', removePlaying);
 }
 
-
-
-
 // Transition End FUnction
 function removePlaying(event) {
     event.target.classList.remove("playing");
 }
-
